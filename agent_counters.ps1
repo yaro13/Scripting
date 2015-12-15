8e8af92c72135bf59878b6e0874ae3246c579524
yaro13 <yaro.frerejean@student.hu.nl>
2015-12-15 12:41:30 +0100

    Temperatuur, RAM, aantal processen
    Temperatuur, RAM, aantal processen toegevoegd en commentaar bijgevoegd

--- agent_counters.ps1
+++ agent_counters.ps1
@@ -1,5 +1,8 @@
 ﻿# Some examples below...
-Get-WmiObject -Class MSAcpi_ThermalZoneTemperature -Namespace root/wmi | select CurrentTemperature
+(Get-WmiObject -Class MSAcpi_ThermalZoneTemperature -Namespace root/wmi | measure-object CurrentTemperature -Average).Average/10-273.15 #gemiddelde temperatuur /10 = kelvin -273.15 = celcius
+(Get-WmiObject Win32_PhysicalMemory | measure-object Capacity -sum).sum/1gb
+(get-service | where {$_.status -eq 'Running'}).count
+
 #ps | measure-object | select count | Out-File C:\scripts\log\agent_counters.txt
 ps | Export-Clixml c:\scripts\log\magweg.xml
 ps | Export-Csv C:\scripts\log\magweg.csv -Force -NoTypeInformation
