88f9d9882170d66fc4c485824477ba293afc023f
yaro13 <yaro.frerejean@student.hu.nl>
2015-12-15 12:00:33 +0100

    Temperatuur CPU toegevoegd
    Met een commando Get-WmiObject gegevens van de CPU opgevraagd, deze
    vervolgens gefilterd met een | select

--- agent_counters.ps1
+++ agent_counters.ps1
@@ -1,8 +1,8 @@
 ﻿# Some examples below...
-
-ps | measure-object | select count | Out-File C:\scripts\log\agent_counters.txt
+Get-WmiObject -Class MSAcpi_ThermalZoneTemperature -Namespace root/wmi | select CurrentTemperature
+#ps | measure-object | select count | Out-File C:\scripts\log\agent_counters.txt
 ps | Export-Clixml c:\scripts\log\magweg.xml
 ps | Export-Csv C:\scripts\log\magweg.csv -Force -NoTypeInformation
 $a = ps | measure-object
 $a.Count
-ps | measure-object | select -expandproperty count
+#ps | measure-object | select -expandproperty count
