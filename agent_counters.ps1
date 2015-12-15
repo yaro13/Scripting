e9edbdecd97f1fd6ade2b0c5949455f13b869da9
yaro13 <yaro.frerejean@student.hu.nl>
2015-12-15 12:58:43 +0100

    Vrije ruimte op schijven in GB
    Vrije ruimte op schijven in GB en commentaar bijgevoegd

--- agent_counters.ps1
+++ agent_counters.ps1
@@ -1,7 +1,9 @@
 ﻿# Some examples below...
 (Get-WmiObject -Class MSAcpi_ThermalZoneTemperature -Namespace root/wmi | measure-object CurrentTemperature -Average).Average/10-273.15 #gemiddelde temperatuur /10 = kelvin -273.15 = celcius
-(Get-WmiObject Win32_PhysicalMemory | measure-object Capacity -sum).sum/1gb
-(get-service | where {$_.status -eq 'Running'}).count
+(Get-WmiObject Win32_PhysicalMemory | measure-object Capacity -sum).sum/1gb #Totaal geheugen op apparaat
+(get-service | where {$_.status -eq 'Running'}).count #aantal draaiende processen
+Get-CimInstance win32_logicaldisk | foreach-object {write " $($_.caption) $('{0:N2}' -f ($_.FreeSpace/1gb)) GB"} #Vrije ruimte op schijven in GB
+
 
 #ps | measure-object | select count | Out-File C:\scripts\log\agent_counters.txt
 ps | Export-Clixml c:\scripts\log\magweg.xml
